aaa3ae00e25c7ec08a5faf053d5f752d4da93e09
HarshAgarwal7202 <harshagarwal7202@gmail.com>
2023-05-13 00:02:44 +0530

    Uploading Code

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./styles/index.css";
+import { DragAndDrop } from "./components";
+function App() {
+  return (
+    <div className="App">
+      {/* <MenuDropDown /> */}
+      {/* <Tabs /> */}
+      {/* <ListBox/> */}
+      {/* <Button/> */}
+      {/* <ModalPopUp/> */}
+      {/* <Toggle/> */}
+      {/* <SideNav /> */}
+      {/* <AddTag/> */}
+      {/* <RadioGroup /> */}
+      {/* <SearchBar/> */}
+      {/* <Filter/> */}
+      {/* <ToolTip /> */}
+      {/* <SearchDropDown/> */}
+      {/* <FormInput/> */}
+      {/* <AddTags/> */}
+      {/* <ColorPicker/> */}
+      {/* <CopyToClipBoard/> */}
+      {/* <Notifications /> */}
+      {/* <SkeletonLoader /> */}
+      {/* <PhoneNumberInput /> */}
+      {/* <Accordion /> */}
+      <DragAndDrop />
+      {/* <Comment /> */}
+      {/* <TabsV2 /> */}
+      {/* <Pagination /> */}
+      {/* <PinToTop /> */}
+    </div>
+  );
+}
+
+export default App;
